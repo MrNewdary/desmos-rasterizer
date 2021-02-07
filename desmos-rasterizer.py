7986d0fdc2c5da223dbf9dfaad3b2889791bbc05
MrNewdary <72687020+MrNewdary@users.noreply.github.com>
2021-02-08 11:42:21 +1300

    Add files via upload

---
+++ desmos-rasterizer.py
@@ -0,0 +1,24 @@
+from PIL import Image
+import pyperclip
+import numpy as np
+from time import sleep
+
+img = Image.open('spooderman.png')
+img = img.resize((1200//20,1577//20))
+pixels = img.load()
+l = [[0]*img.size[1] for i in range(img.size[0])]
+
+for i in range(img.size[0]):
+    for j in range(img.size[1]):
+        l[i][img.size[1]-j-1] = '#%02x%02x%02x' % pixels[i,j][:3]
+
+def command(x,y):
+    return "Calc.setExpression({latex:'("+str(x)+","+str(y)+")', color: '"+l[x][y]+"'})"
+
+s = ''
+
+for i in range(img.size[0]):
+    for j in range(img.size[1]):
+        s = s+command(i,j) + '\n'
+
+pyperclip.copy(s)
